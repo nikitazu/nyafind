e6756b337f1f2ee83c6a3cefc41fcb7a38ed8b1c
Nikita B. Zuev <nikitazu@gmail.com>
2012-05-29 01:32:37 +0400

    Lookup in gui

--- tknya.rb
+++ tknya.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'tk'
+require './nyalib/anime.rb'
+require './nyalib/torrent.rb'
+
 
 root = TkRoot.new { title "Hello, World!" }
 
@@ -10,47 +13,70 @@ TkLabel.new(root) do
 end
 
 titles = TkListbox.new(root) do
-  width 30
-  height 10
+  width 40
+  height 15
+  selectmode :single
   grid :row => 1, :column => 0
 end
 
 currents = TkListbox.new(root) do
   width 5
-  height 10
+  height 15
+  selectmode :single
   grid :row => 1, :column => 1 
 end
 
 totals = TkListbox.new(root) do
   width 5
-  height 10
+  height 15
+  selectmode :single
   grid :row => 1, :column => 2
 end
 
+
 torrents = TkListbox.new(root) do
-  width 30
-  height 5
+  width 40
+  height 15
+  selectmode :single
   grid :row => 2, :column => 0
 end
 
 seeds = TkListbox.new(root) do
   width 5
-  height 5
+  height 15
+  selectmode :single
   grid :row => 2, :column => 1
 end
 
 leechs = TkListbox.new(root) do
   width 5
-  height 5
+  height 15
+  selectmode :single
   grid :row => 2, :column => 2
 end
 
-titles.insert 0, 'Tsukihime', 'Fate/Stay night'
-currents.insert 0, 10, 20
-totals.insert 0, 26, 26
+Anime.load_and_parse('nikitazu') do |anime|
+  titles.insert :end, anime.title
+  currents.insert :end, anime.progress.current
+  totals.insert :end, anime.progress.max
+end
 
-torrents.insert 0, '[Zero-Rws] Tsukihime - 11', '[Horrible] Tsukihime 11'
-seeds.insert 0, 100, 320
-leechs.insert 0, 230, 558
+titles.bind('ButtonRelease-1') do
+  index = titles.curselection[0]
+  title = titles.get(index)
+  series = Integer(currents.get(index))
+  series2 = series + 1
+  query = title.split(' ') << series2
+  
+  torrents.delete 0, :end
+  seeds.delete 0, :end
+  leechs.delete 0, :end
+  
+  Torrent.load_and_parse(query) do |link|
+    torrents.insert :end, link.title
+    seeds.insert :end, link.seed
+    leechs.insert :end, link.leech
+  end
+end
 
 Tk.mainloop
